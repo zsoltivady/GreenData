d6c0c134f083c96cf78d1447738552aa32837ece
patriksalamon95 <patriksalamon95@gmail.com>
2018-11-12 17:40:30 +0100

    a

--- elso1.5/elso/obj/Debug/win3.g.i.cs
+++ elso1.5/elso/obj/Debug/win3.g.i.cs
@@ -67,6 +67,17 @@ namespace elso {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            
+            #line 10 "..\..\win3.xaml"
+            ((System.Windows.Controls.Button)(target)).Click += new System.Windows.RoutedEventHandler(this.Button_Click_2);
+            
+            #line default
+            #line hidden
+            return;
+            }
             this._contentLoaded = true;
         }
     }
