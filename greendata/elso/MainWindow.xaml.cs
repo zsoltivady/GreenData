c7dfd39dbe238fb0328757252e4b42435774bca5
CsaAdam <43348668+CsaAdam@users.noreply.github.com>
2018-11-12 18:46:46 +0100

    Add files via upload

---
+++ greendata/elso/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace elso
+{
+    //BEJELENTKEZÉS 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = @"Server=localhost;Database=kepszerkeszto_db;Uid=root;Pwd='';";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            User.InitializeDB();
+        }
+        public static int bejelentkezve = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 sw = new Window1();
+            sw.Show();
+            this.Close();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            win3 sw = new win3();
+            sw.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string un = textb1.Text;
+            string pw = textb3.Password.ToString();
+
+            if (string.IsNullOrEmpty(un) || string.IsNullOrEmpty(pw))
+            {
+                MessageBox.Show("Minden mező kitöltése kötelező!");
+                textb1.Clear();
+                textb3.Clear();
+            }
+            else
+            {
+
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                MySqlDataAdapter sda = new MySqlDataAdapter("SELECT count(*) FROM users WHERE username='" + un + "'AND password='" + pw + "'", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    MessageBox.Show("Sikeres Bejelentkezés!");
+
+                    win3 w3 = new win3();
+
+                    w3.Show();
+
+                    this.Close();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Hibásan megadott adatok!");
+
+                    textb1.Clear();
+                    textb3.Clear();
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ greendata/elso/win4.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace elso
+{
+    //NAGY KÖZÖS
+    /// <summary>
+    /// Interaction logic for win4.xaml
+    /// </summary>
+    public partial class win4 : Window
+    {
+        public win4()
+        {
+            InitializeComponent();
+            if (MainWindow.bejelentkezve == 1)
+            {
+                bejelentkez.IsEnabled = false;
+                regisztal.IsEnabled = false;
+                fiokbeall.IsEnabled = true;
+                kijelent.IsEnabled = true;
+                    
+            }
+            else
+            {
+                bejelentkez.IsEnabled = true;
+                regisztal.IsEnabled = true;
+                fiokbeall.IsEnabled = false;
+                kijelent.IsEnabled = false;
+                elfogadasravarokepek.IsEnabled = false;
+                megnyit.IsEnabled = false;
+            }
+            
+        }
+
+        /*  win3 sw = new win3();
+            sw.Show();
+            this.Close();
+        */
+        private void cvissza(object sender, RoutedEventArgs e)
+        {
+            win3 sw = new win3();
+            sw.Show();
+            this.Close();
+        }
+        private void cmegnyit(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+            Nullable<bool> result = dlg.ShowDialog();
+        }
+        private void cmentes(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true) ;
+        }
+        private void ckilep(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void ckepkeres(object sender, RoutedEventArgs e)
+        {
+            kepkeres sw = new kepkeres();
+            sw.Show();
+        }
+        private void cbejelentkez(object sender, RoutedEventArgs e)
+        {
+            MainWindow sw = new MainWindow();
+            sw.Show();
+            this.Close();
+        }
+        private void cregisztal(object sender, RoutedEventArgs e)
+        {
+            Window1 sw = new Window1();
+            sw.Show();
+            this.Close();
+        }
+        private void cfiokbeall(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ckijelent(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void celfogadasravarokepek(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cfelhasznaloker(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
