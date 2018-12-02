514f238caa01af02ea3a7f85b24167e10a481a42
CsaAdam <43348668+CsaAdam@users.noreply.github.com>
2018-12-02 14:46:42 +0100

    mappák

---
+++ greendata/elso/Kozos/win4.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace elso
+{
+    //NAGY KÖZÖS
+    /// <summary>
+    /// Interaction logic for win4.xaml
+    /// </summary>
+    public partial class win4 : Window
+    {
+        public win4()
+        {
+            InitializeComponent();
+            if (User.IsLoggedIn()) // BEJELENTKEZETT FELHASZNÁLÓNÁL
+            {
+                bejelentkez.IsEnabled = false;
+                regisztal.IsEnabled = false;
+                fiokbeall.IsEnabled = true;
+                kijelent.IsEnabled = true;
+            }
+            else // VENDÉG FELHASZNÁLÓNÁL 
+            {
+                bejelentkez.IsEnabled = true;
+                regisztal.IsEnabled = true;
+                fiokbeall.IsEnabled = false;
+                kijelent.IsEnabled = false;
+                elfogadasravarokepek.IsEnabled = false;
+                megnyit.IsEnabled = false;
+            }
+        }
+        /*  win3 sw = new win3();
+            sw.Show();
+            this.Close();
+        */
+
+        private void cvissza(object sender, RoutedEventArgs e)
+        {
+            win3 sw = new win3();
+            sw.Show();
+            Close();
+        }
+        private void cmegnyit(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+            bool? result = dlg.ShowDialog();
+        }
+        private void cmentes(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+
+            }
+        }
+        private void ckilep(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        kepkeres sw = new kepkeres();
+        private void ckepkeres(object sender, RoutedEventArgs e)
+        {
+            
+            if (sw.IsClosed)
+            {
+                sw = new kepkeres();
+                sw.Show();
+            }
+            if (!sw.IsLoaded)
+            {
+                sw.Show();
+            }
+            else
+            {
+                sw.Focus();
+            }
+        }
+        private void cbejelentkez(object sender, RoutedEventArgs e)
+        {
+            Login.MainWindow sw = new Login.MainWindow();
+            sw.Show();
+            Close();
+        }
+        private void cregisztal(object sender, RoutedEventArgs e)
+        {
+            Window1 sw = new Window1();
+            sw.Show();
+            Close();
+        }
+        private void cfiokbeall(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ckijelent(object sender, RoutedEventArgs e)
+        {
+            User.UserLogout();
+
+            MessageBox.Show("Sikeres Kijelentkezés!");
+
+            Login.MainWindow sw = new Login.MainWindow();
+
+            Close();
+
+            sw.Show();
+
+        }
+        private void celfogadasravarokepek(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cfelhasznaloker(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ greendata/elso/Valaszto/win3.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace elso
+{
+    //KÉPSZERKESZTŐ/NAGY KÖZÖS
+    /// <summary>
+    /// Interaction logic for win3.xaml
+    /// </summary>
+    public partial class win3 : Window
+    {
+        public win3()
+        {
+            InitializeComponent();
+            if (User.IsLoggedIn())
+            {
+                back_button.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login.MainWindow sw = new Login.MainWindow();
+            sw.Show();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            win4 sw = new win4();
+            sw.Show();
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            Login.MainWindow sw = new Login.MainWindow();
+            sw.Show();
+            Close();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Paint pt = new Paint();
+            pt.Show();
+            Close();
+        }
+    }
+}
