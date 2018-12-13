f762200df10a7e3e25f7268666b935b7dc54c52b
KertaR <KertaR@users.noreply.github.com>
2018-12-13 21:19:49 +0100

    Unit Teszt (Test Username)
    I kívánok you boldog christmas ünnepeket and boldog new évet

---
+++ greendata/elsoTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using elso;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace elsoTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Window1 w1 = new Window1();
+            try
+            {
+                w1.UserName = "Tesztelek";
+            }
+            catch (ArgumentNullException e)
+            {
+                StringAssert.Contains(e.Message, "A mező nem lehet üres");
+                return;
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Minimum 3 karakter hosszúnak kell lennie!");
+                return;
+            }
+            Assert.Fail("Nem találtam hibát");
+        }
+    }
+}
