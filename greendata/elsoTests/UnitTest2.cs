a8643a693cfa42e1c0beb79533e34f051a0da663
CsaAdam <43348668+CsaAdam@users.noreply.github.com>
2019-01-10 08:03:52 +0100

    unit test regisztrációnál jelszó

--- greendata/elsoTests/UnitTest2.cs
+++ greendata/elsoTests/UnitTest2.cs
@@ -8,34 +8,31 @@ using elso;
 
 namespace elsoTests
 {
-    class UnitTest2
+    [TestClass]
+    public class UnitTest2
     {
-        [TestClass]
-        public class UnitTest1
+        [TestMethod]
+        public void TestMethod()
         {
-            [TestMethod]
-            public void TestMethod1()
+            Window1 win1 = new Window1();
+            try
             {
-                Window1 win1 = new Window1();
-                try
-                {
-                    string jelszo = "barack123";
+                string jelszo = "barack123";
 
-                    win1.Password = jelszo;
-                    
-                }
-                catch (ArgumentNullException e)
-                {
-                    StringAssert.Contains(e.Message, "A mező nem lehet üres");
-                    return;
-                }
-                catch (Exception e)
-                {
-                    StringAssert.Contains(e.Message, "Nincs meg a 3 karakter.");
-                    return;
-                }
-                Assert.Fail("nincsen hiba");
+                win1.Password = jelszo;
+
             }
+            catch (ArgumentNullException e)
+            {
+                StringAssert.Contains(e.Message, "A mező nem lehet üres");
+                return;
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Nincs meg a 3 karakter.");
+                return;
+            }
+            Assert.Fail("nincsen hiba");
         }
     }
 }
